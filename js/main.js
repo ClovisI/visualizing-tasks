9e272dde53a35dbf57cd57aec4e0e5b2d1a0a2f9
Patrick <pgronstal@hotmail.com>
2017-07-20 08:58:58 -0600

    First version of Visualizing Tasks homework

---
+++ js/main.js
@@ -0,0 +1,76 @@
+var listItems = [];
+
+// var	 ulItem={
+// 	var taskName = this.taskName,
+// 	assignee: document.getElementByClassName("assignee-radio-list").value,
+// 	difficulty:document.getElementByClassName("difficulty-dropdown").value
+// 	// "this.difficulty"
+// };
+
+var uLItem = {
+	taskName: null,
+	assignee: null,
+	difficulty: null
+};
+
+function addToArray (){
+var newLi = new uLItem(document.getElementsByTagName('li') + "has been assigned the task of" + this.taskName + "which is considered to be a "+ this.difficulty + "task.");
+console.log(newLi);
+taskName: document.getElementsByTagName('textarea'),
+assignee: document.getElementsByTagName('li'),
+difficulty:document.getElementsByTagName('select')
+};
+function createArrayItem (uLItem) = {
+this.taskName = taskName;
+this.taskDescription = taskDescription;
+// this.difficulty-dropdown = difficulty-dropdown;
+return this.uLItem;
+};
+
+function addItem(uLItem){
+// var chart = document.getElementById('chart');
+// document.aside.appendChild(chart.innerHTML(this.uLItem));
+var ul = document.getElementByID("chart");
+var li = document.createElement("li");
+var listLength = listItems.length;
+document.createTextNode(assignee + "has been assigned the task of" + description + "which is considered to be a "+ difficulty + "task.");
+}
+
+// ulItem.addEventListener("click", addItem);
+
+var radioList = document.getElementById("assignee-radio-list");
+console.log(radioList);
+
+function findRadioValue(radioList){
+var radioItems;
+var listNumber;
+
+for (var i = 0; i < array.length; i++) {
+	array[i]
+}
+
+};
+
+//
+// window.onload = function(){
+// 	for (var variable in listItems) {
+// 		if (object.hasOwnProperty(variable)) {
+//
+// 		}
+// 	}
+// };
+//
+//
+// var selected = document.getElementsByTagName('input');
+
+// document.getElementByClassName("").add
+document.getElementById(button.addEventListener('click', selectAssignee, true));
+function selectAssignee(){
+console.log(assignee);
+document.getElementById('chart')
+
+var assignee = document.getElementById('assignee-radio-list	').childNodes.value;
+console.log(assignee);
+var taskDescription = document.getElementsByTagName('textarea');
+var difficulty = document.getElementsByTagName('option').value;
+};
